969a4177b97449ab357ba245c8af29f6400394fb
Eric Thomas <eric@ericthomas.ca>
2023-07-03 18:50:12 -0230

    restore project

--- main.go
+++ main.go
@@ -1,13 +1,205 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
+
+type TodoStatus string
+
+const (
+	TodoStatusPending   TodoStatus = "pending"
+	TodoStatusCompleted TodoStatus = "completed"
+)
+
+type Todo struct {
+	Id          pgtype.UUID `json:"id"`
+	Todo        string      `json:"todo"`
+	Status      TodoStatus  `json:"status"`
+	DateCreated time.Time   `json:"date_created"`
+}
+
+func connectToDatabase() (*pgxpool.Pool, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("error loading .env file: %w", err)
+	}
+
+	pgURI := os.Getenv("PG_URI")
+	if pgURI == "" {
+		return nil, fmt.Errorf("PG_URI environment variable is not set: %w", err)
+	}
+
+	pgxConfig, err := pgxpool.ParseConfig(pgURI)
+	if err != nil {
+		return nil, fmt.Errorf("problem parsing PG_URI: %w", err)
+	}
+
+	pgConnPool, err := pgxpool.NewWithConfig(context.Background(), pgxConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %w", err)
+	}
+
+	return pgConnPool, nil
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	conn, err := connectToDatabase()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error: cannot connect to database", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query(context.Background(), "SELECT id, todo, status, date_created FROM todo")
+	if err != nil {
+		http.Error(w, "Internal server error: unable to execute query", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var todos []Todo
+	for rows.Next() {
+		var todo Todo
+
+		err := rows.Scan(&todo.Id, &todo.Todo, &todo.Status, &todo.DateCreated)
+		if err != nil {
+			http.Error(w, "Internal server error: error occurred during row iteration", http.StatusInternalServerError)
+			return
+		}
+		todos = append(todos, todo)
+	}
+
+	err = json.NewEncoder(w).Encode(todos)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	conn, err := connectToDatabase()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error: cannot connect to database", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	idInput := chi.URLParam(r, "id")
+	id := pgtype.UUID{}
+	err = id.Scan(idInput)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
+		return
+	}
+
+	todo := Todo{}
+	err = conn.QueryRow(context.Background(), "SELECT * FROM todo WHERE id = $1", id).Scan(&todo.Id, &todo.Todo, &todo.Status, &todo.DateCreated)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			http.NotFound(w, r)
+		} else {
+			log.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(todo)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	conn, err := connectToDatabase()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error: cannot connect to database", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	idInput := chi.URLParam(r, "id")
+	id := pgtype.UUID{}
+	err = id.Scan(idInput)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
+		return
+	}
+
+	todo := Todo{}
+	err = conn.QueryRow(context.Background(), "UPDATE todo SET todo = $1, status = $2 WHERE id = $3 RETURNING *", todo.Todo, todo.Status, id).Scan(&todo.Id, &todo.Todo, &todo.Status, &todo.DateCreated)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			http.NotFound(w, r)
+		} else {
+			log.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(todo)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	conn, err := connectToDatabase()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error: cannot connect to database", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	idInput := chi.URLParam(r, "id")
+	id := pgtype.UUID{}
+	err = id.Scan(idInput)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
+		return
+	}
+
+	todo := Todo{}
+	err = conn.QueryRow(context.Background(), "UPDATE todo SET todo = $1, status = $2 WHERE id = $3 RETURNING *", todo.Todo, todo.Status, id).Scan(&todo.Id, &todo.Todo, &todo.Status, &todo.DateCreated)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			http.NotFound(w, r)
+		} else {
+			log.Println(err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(todo)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -15,9 +207,9 @@ func main() {
 	r.Use(middleware.Logger)
 
 	r.Get("/todos", getTodos)
-	r.Get("/todos/{id},", getTodo)
+	r.Get("/todos/{id}", getTodo)
 	r.Post("/todos", createTodo)
-	r.Put("/todos/{id}", updateTodo)
+	r.Put("/todos{id}", updateTodo)
 
 	fmt.Println("Serving on http://localhost:3420...")
 	err := http.ListenAndServe(":3420", r)
